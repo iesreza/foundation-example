98a89cc14c8112e74e12392ba3589959f2ae51ad
Reza <reza@ies-italia.it>
2020-01-30 10:51:35 +0100

    Small fix

--- components/sample/controller.go
+++ components/sample/controller.go
@@ -1,17 +1,19 @@
 package sample
 
-import "github.com/iesreza/foundation/lib/router"
+import (
+	"github.com/iesreza/foundation/lib/router"
+)
 
 func helloWorld() interface{} {
 	return struct {
 		Success bool
 		Message string
 	}{
-		true,"Hello World",
+		true, "Hello World",
 	}
 }
 
 func echo(req router.Request) interface{} {
 	req.Req().ParseForm()
-	return req.Post
+	return req.Req().Form
 }
